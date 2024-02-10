6943d371db4bfa8163b0581e58b4a439a84cd961
Vaness Krylov <i.krylov96@gmail.com>
2024-02-10 18:40:23 +0300

    Add more words

--- src/js/consts.js
+++ src/js/consts.js
@@ -1,2 +1,15 @@
-export const WORDS = ['code', 'frontend', 'programming']
-export const KEYBOARD_LETTERS = Array.from('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+export const WORDS = [
+	'code',
+	'frontend',
+	'programming',
+	'love',
+	'wife',
+	'home',
+	'designer',
+	'engineer',
+	'manager',
+	'junior',
+	'middle',
+	'senior',
+];
+export const KEYBOARD_LETTERS = Array.from('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
